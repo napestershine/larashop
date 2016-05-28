f780f29742fdce36b44e16b7cd444af4c487fe64
napestershine <napestershine@gmail.com>
2016-05-28 09:46:44 +0530

    added new migration employees

--- database/migrations/2016_05_27_194254_create_drinks_table.php
+++ database/migrations/2016_05_27_194254_create_drinks_table.php
@@ -3,17 +3,23 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDrinksTable extends Migration {
+class CreateDrinksTable extends Migration
+{
 
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
-        Schema::create('drinks', function(Blueprint $table) {
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', '75')->unique();
+            $table->text('comments')->nullable();
+            $table->integer('rating');
+            $table->date('brew_date');
+            $table->timestamps();
         });
     }
 
@@ -22,8 +28,9 @@ class CreateDrinksTable extends Migration {
      *
      * @return void
      */
-    public function down() {
-        //
+    public function down()
+    {
+        Schema::drop('drinks');
     }
 
 }
